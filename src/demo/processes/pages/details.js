932b00b1dcd54ce7e5357d3663f2a405342342e7
Vincenzo <vincenzo.villani@studio.unibo.it>
2022-05-01 19:50:56 +0200

    demo process - use layout

--- src/demo/processes/pages/details.js
+++ src/demo/processes/pages/details.js
@@ -1,6 +1,8 @@
 import {Router} from "../router/routing.js"
 import {processModelApi} from "../model/api.js"
 
+import { MainLayout } from "../layouts/base.js"
+
 export const DetailsPage = async (state={})=>{ return {
   div: {
 
@@ -16,87 +18,112 @@ export const DetailsPage = async (state={})=>{ return {
       }
     },
 
-    "=>": [
+    attrs: { style: "width: 100%; height: 100%; padding: 0px; margin: 0px;" },
+
+    css: [
+      `
+      * { box-sizing: border-box !important;}
+
+      body { padding: 0px; margin: 0px; }
+      `
+    ],
+
+    "=>": MainLayout({ 
+
+      NavbarContents: [
+        { div: { text: "Home > Process Details", 'ha.style.fontSize': "2.5rem" }}
+      ],
+
+      SidebarComponents: [
+
+        { div: { 'a.style': {fontWeight: "600", fontSize: "2.5rem", paddingBottom: "0px", marginTop: '25px', marginBottom: '25px'}, text: "Projects"}},
+
+      ], 
+
+      MainContentComponents: [
+        
+        // if any selected
+        { div: { meta: { if: $=>$.scope.selected_project!==undefined},
       
-      // if any selected
-      { div: { meta: { if: $=>$.scope.selected_project!==undefined},
-    
-        props: {
-          project: $=>$.parent.selected_project, //{id: "p0", name: "proj1"},
-          selected_instance_id: $=>$.parent.selected_instance_id, // "i0"
-          
-          instance: undefined
-        },
-        
-        onInit: async $=>{
-          await $.this.api.getInstance()
-        },
-        
-        def: {
-          api: {
-            getInstance: async $=>{
-              $.scope.instance = await processModelApi.getInstance({project_id: $.scope.project.id, instance_id: $.scope.selected_instance_id})
-            },
-            setProcessPointer: async ($, new_process_pointer)=>{
-              await processModelApi.setProcessPointer({project_id: $.scope.project.id, instance_id: $.scope.selected_instance_id, new_process_pointer: new_process_pointer});
-              await $.this.api.getInstance()
-            }
-          }
-        },
-        
-        a: {style: {width: "100%", marginTop: "20px", paddingBottom: "25px"}},
-
-        "=>": [
-
-          { div: { meta: {if: $=>$.scope.instance !== undefined},
-
-            a: {style: {marginBottom: "25px"}},
+          props: {
+            project: $=>$.scope.selected_project, //{id: "p0", name: "proj1"},
+            // selected_instance_id: $=>$.parent.selected_instance_id, // "i0"
             
-            "=>": [
-              {h3: {text: $=>"Project: " + $.scope.project.name, a:{style:{margin: "5px"}}}}, 
-              // {br:{}}, 
-              {h5: {text: $=>"Pocess: " + $.scope.instance.name, a:{style:{margin: "5px"}}}}, 
-            ]
-          }},
+            instance: undefined
+          },
+          
+          onInit: async $=>{
+            await $.this.api.getInstance()
+          },
+          
+          def: {
+            api: {
+              getInstance: async $=>{
+                $.scope.instance = await processModelApi.getInstance({project_id: $.scope.project.id, instance_id: $.scope.selected_instance_id})
+              },
+              setProcessPointer: async ($, new_process_pointer)=>{
+                await processModelApi.setProcessPointer({project_id: $.scope.project.id, instance_id: $.scope.selected_instance_id, new_process_pointer: new_process_pointer});
+                await $.this.api.getInstance()
+              }
+            }
+          },
+          
+          a: {style: {width: "100%", padding: "15px"}},
 
-          { div: { meta: { forEach: "process", of: $=>$.scope.instance?.process || [], define: { index:"process_idx" } },
-            "=>":[
-              { div: { 
-        
-                a: {style: $=>({
-                  width: "60px", height: "60px", display: "inline-block", 
-                  borderRadius: "10px", marginTop: "10px",
-                  color: "white",
-                  backgroundColor: $.meta.process_idx <= $.scope.instance.process_pointer ? "#27ae60" : "#c0392b"
-                })},
-                
-                handle: { onclick: $=>{ 
-                  let pointer = $.scope.instance.process_pointer
-                  if (pointer === $.meta.process_idx){
-                    pointer = $.meta.process_idx-1
-                  }
-                  else {
-                    pointer = $.meta.process_idx
-                  }
-                  $.scope.api.setProcessPointer(pointer)
+          "=>": [
+
+            { div: { meta: {if: $=>$.scope.instance !== undefined},
+
+              
+              "=>": [
+                {h3: {text: $=> $.scope.instance.name, a:{style:{margin: "0px"}}}}, 
+                // {br:{}}, 
+                {h5: {text: $=> $.scope.project.name, a:{style:{margin: "0px", marginTop: "5px"}}}}, 
+              ]
+            }},
+
+            { div: { meta: { forEach: "process", of: $=>$.scope.instance?.process || [], define: { index:"process_idx" } },
+              
+              a: {style: {marginTop: "25px"}},
+
+              "=>":[
+                { div: { 
+          
+                  a: {style: $=>({
+                    width: "60px", height: "60px", display: "inline-block", 
+                    borderRadius: "10px", marginTop: "10px",
+                    color: "white",
+                    backgroundColor: $.meta.process_idx <= $.scope.instance.process_pointer ? "#27ae60" : "#c0392b"
+                  })},
+                  
+                  handle: { onclick: $=>{ 
+                    let pointer = $.scope.instance.process_pointer
+                    if (pointer === $.meta.process_idx){
+                      pointer = $.meta.process_idx-1
+                    }
+                    else {
+                      pointer = $.meta.process_idx
+                    }
+                    $.scope.api.setProcessPointer(pointer)
+                  }},
+                  
+                  text: { div: { a:{style:"width: 100%; height: 100%; display:flex; justify-content: center; align-items: center"}, text: $ => $.meta.process}},
+                  
                 }},
-                
-                text: { div: { a:{style:"width: 100%; height: 100%; display:flex; justify-content: center; align-items: center"}, text: $ => $.meta.process}},
-                
-              }},
 
-              { input: {a:{style:"margin-left: 15px; border-top: none; border-left:none; border-right:none; width: 200px", value: "an example of some note!"}}}
-            ]
-          }},
-        ]
-        
-      }},
+                { input: {a:{style:"margin-left: 15px; border-top: none; border-left:none; border-right:none; width: 200px; background: transparent", value: "an example of some note!"}}}
+              ]
+            }},
+          ]
+          
+        }},
 
-      // else
-      { div: { meta: { if: $=>$.scope.selected_project===undefined},
-        text: "nothing selected.."
-      }}
+        // else
+        { div: { meta: { if: $=>$.scope.selected_project===undefined},
+          text: "nothing selected.."
+        }} 
 
-    ]
+      ]
+    })
   }
 }}
--- src/demo/processes/pages/home.js
+++ src/demo/processes/pages/home.js
@@ -4,6 +4,8 @@ import {processModelApi} from "../model/api.js"
 
 import { Steps, SimpleSteps } from "../components/steps.js"
 
+import { MainLayout } from "../layouts/base.js"
+
 export const HomePage = async (state)=>{ return { 
   div: {
     id: "home",
@@ -36,52 +38,79 @@ export const HomePage = async (state)=>{ return {
       `
     ],
     
-    "=>": [
+    "=>": MainLayout({ 
+      NavbarContents: [
+        { div: { text: "Home", 'ha.style.fontSize': "2.5rem" }}
+      ],
+      SidebarComponents: [
 
-      // PROJECTS
-      { div: { meta: {forEach:"project", of: $=>$.scope.projects || []},
+        { div: { 'a.style': {fontWeight: "600", fontSize: "2.5rem", paddingBottom: "0px", marginTop: '25px', marginBottom: '25px'}, text: "Projects"}},
 
-        attrs: {style: { borderRadius: "5px", backgroundColor:"#2c3e50", color: "white", margin: "20px", padding: "5px 15px 15px 15px"}},
-
-        "=>": [
-
-          { h4: { text: $=>$.meta.project.name, a: {style: {margin:"0px", marginBottom: "10px"}} }},
-
-          { li: { meta: {forEach:"instance", of: $=>$.meta.project?.instances || []},
-            
-            handle: {
-              onclick: $=>{
-                console.log($.meta.instance.id, )
-                // $.scope.selected_instance_id = $.meta.instance.id
-                // $.scope.selected_project = $.meta.project
-
-                // setTimeout(()=>
-                  Router.navigate("details", {
-                    selected_instance_id: $.meta.instance.id,
-                    selected_project: $.meta.project
-                  })
-                // , 3000)
-              }
-            },
+        { div: { meta: {forEach:"project", of: $=>$.scope.projects || []},
+            'ha.style.marginBottom': "25px",
 
             "=>": [
-              { span: { text: $=>$.meta.instance.name }},
+                { div: { text: $=>$.meta.project.name, a: {style: {fontWeight: "600", fontSize: "1.5rem", marginBottom: "10px"}} }},
 
-              Extended(Steps, {
-                props: {
-                  num_steps: $=>$.meta.instance.todo,
-                  step_idx: $=>$.meta.instance.done,
-                },
-                a: { style: {display: "inline-block", margin: "0 10px"}}
-              }),
-
-              { span: { text: $=>"(" +$.meta.instance.done + "/" + $.meta.instance.todo +")"}},
+                { li: { meta: {forEach:"instance", of: $=>$.meta.project?.instances || []},
+                    'ha.style.fontWeight': "500",
+                    'ha.style.fontSize': "1.2rem",
+                    'ha.style.marginLeft': "10px",
+                    "=>": { span: { text: $=>$.meta.instance.name }},
+                }}
             ]
-          }}
-        ]
-        
-      }},
 
-    ]
+        }}
+      ],
+      
+      MainContentComponents:[
+
+        // PROJECTS
+        { div: { meta: {forEach:"project", of: $=>$.scope.projects || []},
+
+          attrs: {style: { borderRadius: "5px", backgroundColor:"#2c3e50", color: "white", margin: "20px", padding: "5px 15px 15px 15px"}},
+
+          "=>": [
+
+            { h4: { text: $=>$.meta.project.name, a: {style: {margin:"0px", marginBottom: "10px"}} }},
+
+            { li: { meta: {forEach:"instance", of: $=>$.meta.project?.instances || []},
+              
+              handle: {
+                onclick: $=>{
+                  console.log($.meta.instance.id, )
+                  // $.scope.selected_instance_id = $.meta.instance.id
+                  // $.scope.selected_project = $.meta.project
+
+                  // setTimeout(()=>
+                    Router.navigate("details", {
+                      selected_instance_id: $.meta.instance.id,
+                      selected_project: $.meta.project
+                    })
+                  // , 3000)
+                }
+              },
+
+              "=>": [
+                { span: { text: $=>$.meta.instance.name }},
+
+                Extended(Steps, {
+                  props: {
+                    num_steps: $=>$.meta.instance.todo,
+                    step_idx: $=>$.meta.instance.done,
+                  },
+                  a: { style: {display: "inline-block", margin: "0 10px"}}
+                }),
+
+                { span: { text: $=>"(" +$.meta.instance.done + "/" + $.meta.instance.todo +")"}},
+              ]
+            }}
+          ]
+          
+        }},
+      
+      ]
+
+    })
   }
 }}
